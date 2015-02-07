1c40f6a88d0968cbde42d4341491976d3ecf9e63
smmoreno <samuel_moreno@sbcglobal.net>
2015-02-06 22:11:44 -0800

    Lab 4 Assignment for CSE455

---
+++ CSE455-lab4/app/src/main/java/edu/csusb/cse455_lab4/MainActivity.java
@@ -0,0 +1,20 @@
+package edu.csusb.cse455_lab4;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import cse520.helloworld.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
